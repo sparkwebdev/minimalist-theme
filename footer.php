661f57dc1a80ca17dfbaa92c4816f75dd64cb0dd
Steven Park <info@stevenpark.co.uk>
2018-10-26 23:46:12 +0100

    Header and Footer Logos

--- footer.php
+++ footer.php
@@ -13,18 +13,9 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="Glasgow Bike Store logo" class="site-footer-logo" />
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'minimalist-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'minimalist-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'minimalist-theme' ), 'minimalist-theme', '<a href="https://carmemias.com">carmemias</a>' );
-				?>
+			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- header.php
+++ header.php
@@ -24,21 +24,26 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo-text.png" alt="Glasgow Bike Store logo" class="site-header-logo" />
+				<div class="screen-reader-text">
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$minimalist_theme_description = get_bloginfo( 'description', 'display' );
-			if ( $minimalist_theme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $minimalist_theme_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
+				if ( is_front_page() && is_home() ) :
+					?>
+					<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+					<?php
+				else :
+					?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+				endif;
+				$minimalist_theme_description = get_bloginfo( 'description', 'display' );
+				if ( $minimalist_theme_description || is_customize_preview() ) :
+					?>
+					<p class="site-description"><?php echo $minimalist_theme_description; /* WPCS: xss ok. */ ?></p>
+				<?php endif; ?>
+				</div>
+			</a>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
